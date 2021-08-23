3b57ed9b4c9e44fbe8aca88ed993b0842b96a4ba
Toly <teran4work@gmail.com>
2021-08-23 16:42:57 +0300

    Brought to working state

--- src/DrawioResourceLoader.ts
+++ src/DrawioResourceLoader.ts
@@ -1,4 +1,5 @@
 export class DrawioResourceLoader {
+    private DRAWIO_DIR = 'public/drawio/src/main/webapp';
     private graphEditorResources = [
         'Editor.js',
         'EditorUi.js',
@@ -69,7 +70,35 @@ export class DrawioResourceLoader {
     ];
     private static instance: DrawioResourceLoader;
     private promise: Promise<Window> | null = null;
-    private constructor() {}
+    private constructor() {
+        window.uiTheme = 'light';
+        window.urlParams = window.urlParams || {};
+        window.isLocalStorage = window.isLocalStorage || false;
+        window.mxLoadSettings = window.mxLoadSettings || urlParams['configure'] != '1';
+
+        window.isSvgBrowser =
+            window.isSvgBrowser || navigator.userAgent.indexOf('MSIE') < 0 || document.documentMode >= 9;
+        window.VIEWER_URL = null;
+        window.SHAPES_PATH = `${this.DRAWIO_DIR}/shapes`;
+        window.TEMPLATE_PATH = `${this.DRAWIO_DIR}/templates`;
+        window.RESOURCES_PATH = `${this.DRAWIO_DIR}/resources`;
+        window.RESOURCE_BASE = window.RESOURCES_PATH + '/grapheditor';
+        window.mxLoadResources = window.mxLoadResources || false;
+        window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE || 10485760;
+        window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
+
+        window.STENCIL_PATH = `${this.DRAWIO_DIR}/stencils`;
+        window.STYLE_PATH = `${this.DRAWIO_DIR}/styles`;
+        window.IMAGE_PATH = `${this.DRAWIO_DIR}/images`;
+        window.GRAPH_IMAGE_PATH = `${this.DRAWIO_DIR}/img`;
+        window.CSS_PATH = `${this.DRAWIO_DIR}/styles`;
+        window.OPEN_FORM = window.OPEN_FORM || 'open.html';
+        window.mxBasePath = `${this.DRAWIO_DIR}/mxgraph`;
+        window.mxImageBasePath = `${this.DRAWIO_DIR}/mxgraph/images`;
+        window.mxLanguage = 'ru';
+        window.mxLanguages = window.mxLanguages || ['ru', 'en'];
+        window.mxSettings = {};
+    }
     private async injectScript(filePath: string) {
         const script = document.createElement('script');
         script.type = 'text/javascript';
@@ -86,6 +115,23 @@ export class DrawioResourceLoader {
         });
         return loadPromise;
     }
+    private async injectStyle(filePath: string) {
+        const style = document.createElement('link');
+        style.rel = 'stylesheet';
+        style.type = 'text/css';
+        style.href = filePath;
+        const loadPromise = new Promise((resolve, reject) => {
+            style.addEventListener(
+                'load',
+                function () {
+                    resolve(style);
+                },
+                false,
+            );
+            document.getElementsByTagName('head')[0].appendChild(style);
+        });
+        return loadPromise;
+    }
     public static getInstance(): DrawioResourceLoader {
         if (!DrawioResourceLoader.instance) {
             DrawioResourceLoader.instance = new DrawioResourceLoader();
@@ -95,20 +141,30 @@ export class DrawioResourceLoader {
     }
 
     public async load() {
-        console.time();
         if (this.promise) return this.promise;
+        console.time();
+
         this.promise = new Promise(async (resolve) => {
-            await this.injectScript('public/drawio/mxgraph/mxClient.js');
+            await this.injectScript(`${this.DRAWIO_DIR}/js/deflate/pako.min.js`);
+            await this.injectScript(`${this.DRAWIO_DIR}/js/deflate/base64.js`);
+            await this.injectScript(`${this.DRAWIO_DIR}/js/jscolor/jscolor.js`);
+            await this.injectScript(`${this.DRAWIO_DIR}/js/jscolor/jscolor.js`);
+            await this.injectScript(`${this.DRAWIO_DIR}/js/sanitizer/sanitizer.min.js`);
+            await this.injectScript(`${this.DRAWIO_DIR}/mxgraph/mxClient.js`);
             await Promise.all(
-                this.graphEditorResources.map((name) => this.injectScript(`public/drawio/js/grapheditor/${name}`)),
+                this.graphEditorResources.map((name) => this.injectScript(`${this.DRAWIO_DIR}/js/grapheditor/${name}`)),
             );
 
             await Promise.all(
                 this.sidebarResources.map((name) =>
-                    this.injectScript(`public/drawio/js/diagramly/sidebar/${name}`),
+                    this.injectScript(`${this.DRAWIO_DIR}/js/diagramly/sidebar/${name}`),
                 ),
             );
-
+            await this.injectStyle(`${this.DRAWIO_DIR}/mxgraph/css/common.css`)
+            await this.injectStyle(`${this.DRAWIO_DIR}/mxgraph/css/explorer.css`)
+            await this.injectStyle(`${this.DRAWIO_DIR}/styles/atlas.css`)
+            await this.injectStyle(`${this.DRAWIO_DIR}/styles/grapheditor.css`)
+            
             fetch(RESOURCES_PATH + '/dia_ru.txt')
                 .then((resp) => resp.text())
                 .then((resource) => {
